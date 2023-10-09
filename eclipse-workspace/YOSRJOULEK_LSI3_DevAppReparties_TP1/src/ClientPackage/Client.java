d23a329b07c667ec1f38867c083875ceace0e33f
yosr <joulekyosr123@gmail.com>
2023-10-09 21:26:39 +0100

    first commit

--- eclipse-workspace/YOSRJOULEK_LSI3_DevAppReparties_TP1/src/ClientPackage/Client.java
+++ eclipse-workspace/YOSRJOULEK_LSI3_DevAppReparties_TP1/src/ClientPackage/Client.java
@@ -3,26 +3,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.util.Scanner;
 import java.net.InetAddress;
 import java.net.*;
 public class Client {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Je suis un client pas encore connecté");
+        int serverPort = 1234; 
+        try {
+        	
+            InetAddress host = InetAddress.getByName("localhost");
+            Socket socket = new Socket(host, 1234); 
+            System.out.println("Je suis un client connecté");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	public static void main(String[] args)throws IOException {
-		// TODO Auto-generated method stub
-		System.out.println("je suis  un client pas encore connecte");
-		InetAddress host =InetAddress.getByName("localhost");
-		try {
-			Socket socket = new Socket(host,1234);
-		} 
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("je suis  un client connecte");
-	
-	
-        
-	}
-	}
+        System.out.println("Je suis un client connecté");
+    }
+}
 
 
--- eclipse-workspace/YOSRJOULEK_LSI3_DevAppReparties_TP1/src/ServerPackage/Server.java
+++ eclipse-workspace/YOSRJOULEK_LSI3_DevAppReparties_TP1/src/ServerPackage/Server.java
@@ -7,17 +7,19 @@ import java.net.Socket;
 public class Server {
 
 	public static void main(String[] args) throws IOException{
+		int serverPort=1234;
+		ServerSocket ss=new ServerSocket(serverPort);
 		// TODO Auto-generated method stub
-		int ServerPort=1234;
-		ServerSocket ss=new ServerSocket(ServerPort);
-		System.out.println("je suis un serveur en attente de la connexion d'un client");
-		try {
-		Socket socket =ss.accept();
-		System.out.println("un client est connecte");
-		socket.close();
-        
-	}catch(Exception e) {e.printStackTrace();}
-		
-	}
+      System.out.println("je suis un serveur en attente la connexion d'un client");
+      try {
+      Socket socket=ss.accept();
+      System.out.println("un client est connecte");
+      socket.close();
+	 } 
+	catch (Exception e) {e.printStackTrace();}
 
+	}
+    
+
+	
 }
